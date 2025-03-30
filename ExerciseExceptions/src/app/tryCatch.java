5c40abcc141528f6386abee69b8f82cd564a4ab9
Rafael Elbl <rafael-elbl@hotmail.com>
2025-03-30 17:04:41 -0300

    simple error treatment

---
+++ ExerciseExceptions/src/app/tryCatch.java
@@ -0,0 +1,26 @@
+package app;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class tryCatch {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Digite os elementos separados por espaço: ");
+            String[] vect = sc.nextLine().split(" ");
+            System.out.println("Digite a posição: ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e1) {
+            System.out.println("Digite um número válido");
+        } catch (InputMismatchException e2) {
+            System.out.println("Não é um número");
+        }
+
+        sc.close();
+    }
+
+}
---
+++ java/src/application/excecoes.java
@@ -0,0 +1,16 @@
+package application;
+
+public class excecoes {
+
+    public static void main(String[] args) {
+
+        /*
+        * IOException: erro entrada e saída
+        * RuntimeException: não precisa tratar
+        *  - IndexOutOfBoundsException: tenta acessar posicao array q nao existe
+        *  - NullPointerException: tenta acessar uma variável nula
+        * */
+
+    }
+
+}
