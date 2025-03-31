177cdb1c87b931976cefdf104636c2ad20a5b399
Rafael Elbl <rafael-elbl@hotmail.com>
2025-03-31 20:19:50 -0300

    Leitura de arquivo na forma correta

--- java/src/application/archives.java
+++ java/src/application/archives.java
@@ -1,6 +1,10 @@
 package application;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -8,22 +12,22 @@ public class archives {
 
     public static void main(String[] args) {
 
-        File file = new File("C:\\Program Files\\in.txt");
-        Scanner sc = null;
+        String path = "C:\\Program Files\\in.txt";
 
-        try {
-            sc = new Scanner(file);
-            //ENQUANTO O ARQUIVO TIVER UMA NOVA LINHA
-            while (sc.hasNextLine()) {
-                //LEIA ESSA LINHA E PRINTE
-                System.out.println(sc.nextLine());
+        //FILEREADER = LEITOR DE ARQUIVO
+        //BUFFEREDREADER = DEIXA A LEITURA MAIS RÁPIDA
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            //CRIA UM LEITOR DE LINHA NA VARIÁVEL
+            String line = br.readLine();
+
+            //ENQUANTO A LINHA NÃO FOR NULL (ARQUIVO ACABADO) FAZ O PRINT E LÊ A LINHA
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
             }
+
         } catch (IOException e) {
             System.out.println("ERRO: " + e.getMessage());
-        } finally {
-            if (sc != null) {
-                sc.close();
-            }
         }
 
     }
