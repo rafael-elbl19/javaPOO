eb7e2329b7a6e3eabde097fe5ff9192d2cb3e261
Rafael Elbl <rafael-elbl@hotmail.com>
2025-04-06 15:55:47 -0300

    Mesmo exemplo, mas agora utilizando curinga para diferenciar as listas

--- CuringasDelimitados/src/model/applications/Main.java
+++ CuringasDelimitados/src/model/applications/Main.java
@@ -10,16 +10,24 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Shape> myShapes = new ArrayList<>();
-        myShapes.add(new Rectangle(2.0, 2.0));
-        myShapes.add(new Circle(3.0));
+        List<Circle> myCircles = new ArrayList<>();
+        myCircles.add(new Circle(2.0));
+        myCircles.add(new Circle(3.0));
 
+        List<Rectangle> myRectangles = new ArrayList<>();
+        myRectangles.add(new Rectangle(2.0, 2.0));
+        myRectangles.add(new Rectangle(3.0, 3.0));
 
-        System.out.println("Soma total das áreas: " + String.format("%.2f", totalArea(myShapes)));
+        System.out.println("Soma total das áreas dos círculos: " + String.format("%.2f", totalArea(myCircles)));
+        System.out.println("Soma total das áreas dos retângulos: " + String.format("%.2f", totalArea(myRectangles)));
 
     }
 
-    public static double totalArea(List<Shape> list) {
+    /*POR QUE OCORRE?
+    * Mesmo o tipo Circle sendo subclasse do tipo Shape, a List Circle não é subclasse da List Shape
+    * Está dizendo "Aceito uma lista de qualquer coisa que estenda Shape"
+    * */
+    public static double totalArea(List<? extends Shape> list) {
         double sum = 0.0;
         for (Shape s : list) {
             sum+=s.area();
