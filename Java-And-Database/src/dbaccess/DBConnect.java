7142d84d33ad20ded8d473b3e580c79d02f75d6d
Rafael Elbl <rafael-elbl@hotmail.com>
2025-04-19 11:36:15 -0300

    Using JDBC API to recover data from the database

--- Java-And-Database/src/dbaccess/DBConnect.java
+++ Java-And-Database/src/dbaccess/DBConnect.java
@@ -11,7 +11,7 @@ public class DBConnect {
 		Connection conn = null;
 		
 		try {
-			String url = "jdbc:mysql://localhost:3306/usersdb?user=root&password=1122003a&useSSL=false&allowPublicKeyRetrieval=true";
+			String url = "jdbc:mysql://localhost:3306/coursejdbc?user=root&password=1122003a&useSSL=false&allowPublicKeyRetrieval=true";
 			conn = DriverManager.getConnection(url); //Verifica um driverJDBC compatível e conecta à base de dados da url
 			
 		} 
